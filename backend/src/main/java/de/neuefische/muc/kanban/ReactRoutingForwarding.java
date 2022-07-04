1aedc42ae1dbf27ee6cdaa221b76d7cdbf1cd5fc
André Schreck <moin@andreschreck.de>
2022-07-04 13:50:15 +0200

    forward to react app

---
+++ backend/src/main/java/de/neuefische/muc/kanban/ReactRoutingForwarding.java
@@ -0,0 +1,13 @@
+package de.neuefische.muc.kanban;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactRoutingForwarding {
+
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String forwardToRouteUrl() {
+        return "forward:/";
+    }
+}
