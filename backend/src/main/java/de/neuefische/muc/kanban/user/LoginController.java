ecb0194e2394601b0b455d23f29b006479786c67
André Schreck <moin@andreschreck.de>
2022-07-05 12:54:24 +0200

    only catch AuthenticationException

--- backend/src/main/java/de/neuefische/muc/kanban/user/LoginController.java
+++ backend/src/main/java/de/neuefische/muc/kanban/user/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,7 @@ public class LoginController {
             KanbanUser user = userService.findByUsername(loginData.getUsername()).orElseThrow();
             String jwt = jwtService.createJwt(new HashMap<>(), user.getId());
             return ResponseEntity.ok(new LoginResponse(jwt));
-        } catch (Exception e) {
+        } catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
